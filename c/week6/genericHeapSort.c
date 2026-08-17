1a9b2b271f20ccbc43bf96226157030fef1b39b3
Sunaina-04 <sunaina04sharma@gmail.com>
2026-08-17 15:33:21 +0530

    genericHeapSort file

--- c/week6/genericHeapSort.c
+++ c/week6/genericHeapSort.c
@@ -43,15 +43,12 @@ void quick_sort(void *a, int L, int R, int (*cmp)(void *, void *), int sz)
     quick_sort(a, x + 1, R, cmp, sz);
 }
 
-void print(int *a, int s)
-{
-    printf("print");
-    int i;
-    for (i = 0; i < s; i++)
-    {
-        printf("%d ", *(a + i));
-    }
-    printf("\n");
+void printArray(int *array, int size) {
+	printf("[");
+	for(int i = 0; i < size; i++) {
+		printf(" %d ", arr[i]);
+	}
+	printf("]\n");
 }
 
 struct student
@@ -91,49 +88,101 @@ int studentIdCmp(void *i, void *j)
     return x->id - y->id;
 }
 
-int main()
-{
-    int size = 8;
-    int a[size];
 
-    generateRandom(a, size);
-    print(a, size);
+void swap (int *heap, int pos1, int pos2) {
+	int temp = heap[pos1];
+	heap[pos1] = heap[pos2];
+	heap[pos2] = temp;
+}
 
-    quick_sort(a, 0, size - 1, intCmparator, sizeof(int));
+void sink(int *heap, int size, int curr) {
+	int root = curr;
+	int left = curr * 2 + 1;
+	int right = curr * 2 + 2;
 
-    print(a, size);
+	if (left < size && heap[left] > heap[root]) {
+		root = left;
+	}
+	if (right < size && heap[right] > heap[root]) {
+		root =  right;
+	}
 
-    float f[6] = {1.2, 3.4, .7, .8, .4, .3};
-    quick_sort(f, 0, 5, floatCmparator, sizeof(float));
-    int i;
-    for (i = 0; i < 6; i++)
-    {
-        printf("%f ", f[i]);
-    }
+	if (curr != root) {
+		swap(heap, curr, root);
+		sink(heap, size, root);
+	}
+}
 
-    int n = 7;
-    struct student *d = (struct student *)(malloc(sizeof(struct student *) * n));
 
-    float cgpa[] = {1.2f, 2.2f, 1.3f, .7f, 5.4f, 2.3f, .9f};
-    char *names[] = {"ram", "tina", "tom", "sam", "tom", "david", "harry"};
-    for (i = 0; i < n; i++)
-    {
-        struct student *s = (struct student *)(malloc(sizeof(struct student *)));
-        s->id = rand() % 100;
-        s->cgpa = cgpa[i];
-        s->name = names[i];
-        d[i] = *s;
-    }
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d:%s:%.2f, ", d[i].id, d[i].name, d[i].cgpa);
-    }
-    quick_sort(d, 0, 6, studentIdCmp, sizeof(struct student *));
+	// maybe the pointer required to be used is void 
+void heapSort(int *heap, int size) {
+	
+	for (int i = (size/2) -1; i >= 0; i--) {
+		sink(heap, size, current);
+	}
 
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d:%s:%.2f, ", d[i].id, d[i].name, d[i].cgpa);
-    }
+	for (int i = size - 1; i >=0; i--) {
+		swap(heap, 0, i);
 
-    return 0;
+		sink(heap, i, 0);
+	}
+}
+
+int main() {
+
+	// int array 
+	int arr[] = {5,9,4,2.8,0};
+	int size = sizeof(arr) / sizeof(arr[0]);
+
+	generateRandom(arr, size);
+	
+	// print array 
+	printArray(arr, size);
+	
+
+	// heapSort 
+	heapSort(arr, size);
+	print(a, size);
+
+	// float Array 
+	float f[6] = {1.2, 3.4, .7, .8, .4, .3};
+    //quick_sort(f, 0, 5, floatCmparator, sizeof(float));
+    
+	//should print float array --> printArray()
+	int i;
+    	for (i = 0; i < 6; i++)
+    	{
+        	printf("%f ", f[i]);
+    	}
+
+	// Struct data type 
+   	 int n = 7;
+   	 struct student *d = (struct student *)(malloc(sizeof(struct student *) * n));
+
+    	float cgpa[] = {1.2f, 2.2f, 1.3f, .7f, 5.4f, 2.3f, .9f};
+    	char *names[] = {"ram", "tina", "tom", "sam", "tom", "david", "harry"};
+   	for (i = 0; i < n; i++)
+    	{
+        	struct student *s = (struct student *)(malloc(sizeof(struct student *)));
+        	s->id = rand() % 100;
+        	s->cgpa = cgpa[i];
+        	s->name = names[i];
+        	d[i] = *s;
+    	}
+    	for (int i = 0; i < n; i++)
+    	{
+        	printf("%d:%s:%.2f, ", d[i].id, d[i].name, d[i].cgpa);
+    	}
+    	
+	// heap Sort 	
+//	quick_sort(d, 0, 6, studentIdCmp, sizeof(struct student *));
+
+    	
+	// print heap
+	for (int i = 0; i < n; i++)
+    	{
+        	printf("%d:%s:%.2f, ", d[i].id, d[i].name, d[i].cgpa);
+    	}
+
+    	return 0;
 }
